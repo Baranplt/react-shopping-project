cb2021eb304f723088270fc084f0cdc593699464
Baran Polat <baranpolat@hotmail.com.tr>
2023-01-10 23:44:53 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, useLocation } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/reducers/index'
 
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Wrapper>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Wrapper>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
